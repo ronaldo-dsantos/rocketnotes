1213cce0c01ce3369268b8de8ac89cfeaee90bb0
ronaldo <ronaldo.dsantos@gmail.com>
2024-06-20 00:59:54 -0300

    Created ButtonText & Tag

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,8 +1,11 @@
-import { Container, Links } from "./styles" // Importando o arquivo de estilos css
+import { Container, Links, Content } from "./styles" // Importando o arquivo de estilos css
 
 import { Header } from "../../components/Header" // Importando o Header
 import { Button } from "../../components/Button"
 import { Section } from "../../components/Section"
+import { Tag } from "../../components/Tag"
+import { ButtonText } from "../../components/ButtonText"
+
 
 export function Details(){
  
@@ -10,22 +13,40 @@ export function Details(){
     <Container>
       <Header />
 
-      <Section title="Links úteis">
-        <Links>
-          <li>
-            <a href="#">https://www.rocketseat.com.br/</a>
-          </li>
-          <li>
-            <a href="#">https://www.rocketseat.com.br/</a>
-          </li>
-        </Links>
-      </Section>
+      <main>
+        <Content>
+          <ButtonText title="Excluir nota" />
 
-      <Section title="Links úteis">
-        
-      </Section>
+          <h1>Introdução ao React</h1>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+            when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+            It has survived not only five centuries, but also the leap into electronic typesetting, 
+            remaining essentially unchanged. 
+            It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+            and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+          </p>
 
-      <Button title="Voltar" />
+          <Section title="Links úteis">
+            <Links>
+              <li>
+                <a href="#">https://www.rocketseat.com.br/</a>
+              </li>
+              <li>
+                <a href="#">https://www.rocketseat.com.br/</a>
+              </li>
+            </Links>
+          </Section>
+
+          <Section title="Marcadores">
+            <Tag title="express"/>
+            <Tag title="nodejs"/>
+          </Section>
+
+          <Button title="Voltar" />
+        </Content>
+      </main>
     </Container>
   )
 }
--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -6,9 +6,15 @@ export const Container = styled.div`
 
   display: grid; // Display grid para deixarmos o Header fixo na tela
   grid-template-rows: 105px auto; // Header 105px e o restante da página ajuste automático
-  grid-template-areas: // Nomeando as áreas do grid
+  grid-template-areas: 
   "header"
-  "content";  
+  "content";
+  
+  > main {
+    grid-area: "Content";
+    overflow-y: scroll;
+    padding: 64px 0;
+  }
 `
 
 export const Links = styled.ul`
@@ -23,4 +29,26 @@ export const Links = styled.ul`
   }
 `
 
+export const Content = styled.div`
+  max-width: 550px;
+  margin: 0 auto;
 
+  display: flex;
+  flex-direction: column;
+
+  > button:first-child {
+    align-self: end; // Alinhamento no próprio botão trazendo ele para o final
+  }
+
+  > h1 {
+    font-size: 36px;
+    font-weight: 500;
+    padding-top: 64px;
+  }
+
+  > p {
+    font-size: 16px;
+    margin-top: 16px;
+    text-align: justify;
+  }
+`
