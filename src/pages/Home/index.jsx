03098afc9be709f0cecfe5c840bca8fe1479cfce
ronaldo <ronaldo.dsantos@gmail.com>
2024-06-21 01:03:37 -0300

    Created ButtonText & Home

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,40 @@
+import { FiPlus, FiSearch} from 'react-icons/fi'
+
+import { Container, Brand, Menu, Search, Content, NewNote } from "./styles"
+
+import { Header } from '../../components/Header'
+import { ButtonText } from '../../components/ButtonText'
+import { Input } from '../../components/Input'
+
+export function Home(){
+  return(
+    <Container>
+      <Brand>
+        <h1>Rocketnotes</h1>
+      </Brand>
+
+      <Header />
+
+      <Menu>
+        <li><ButtonText title="Todos" isActive/></li>
+        <li><ButtonText title="Frontend" /></li>
+        <li><ButtonText title="Node" /></li>
+        <li><ButtonText title="React" /></li>
+      </Menu>
+
+      <Search>
+        <Input placeholder="Pesquisar pelo título" />
+      </Search>
+
+      <Content>
+
+      </Content>
+
+      <NewNote>
+        <FiPlus />
+        Criar nota
+      </NewNote>
+
+    </Container>
+  )
+}
