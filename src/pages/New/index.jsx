ae3f988a4307f2792f9525d9cb7d79244a44353c
ronaldo <ronaldo.dsantos@gmail.com>
2024-06-25 00:20:41 -0300

    Created Components NoteItem

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -3,6 +3,9 @@ import { Container, Form } from "./styles"
 import { Header } from "../../components/Header"
 import { Input } from "../../components/Input"
 import { Textarea } from "../../components/Textarea"
+import { NoteItem } from "../../components/NoteItem"
+import { Section } from "../../components/Section"
+
 
 export function New(){
   return(
@@ -20,6 +23,11 @@ export function New(){
           <Input placeholder="Título" />
 
           <Textarea placeholder="Observações" />
+
+          <Section title="Links úteis">
+            <NoteItem value="https://" />
+            <NoteItem isNew placeholder="Novo link"/>
+          </Section>
         </Form>
       </main>
     </Container>
