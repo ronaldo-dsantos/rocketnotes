a6b4f9875b55bf4a05a06515efa49126e6c940af
ronaldo <ronaldo.dsantos@gmail.com>
2024-07-24 23:33:00 -0300

    Adjusting navigation features

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { Link, useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 
 import { Container, Form } from "./styles"
 
@@ -11,6 +11,7 @@ import { Textarea } from "../../components/Textarea"
 import { NoteItem } from "../../components/NoteItem"
 import { Section } from "../../components/Section"
 import { Button } from "../../components/Button"
+import { ButtonText } from "../../components/ButtonText"
 
 export function New(){
   const [title, setTitle] = useState("")
@@ -23,6 +24,10 @@ export function New(){
   const [newTag, setNewTag] = useState("")
 
   const navigate = useNavigate()
+
+  function handleBack() {
+    navigate(-1)
+  }
 
   function handleAddLink() {
     setLinks(prevState => [...prevState, newLink])
@@ -63,7 +68,7 @@ export function New(){
     })
 
     alert("Nota cadastrada com sucesso!")
-    navigate("/")
+    navigate(-1)
   }
 
   return(
@@ -75,7 +80,10 @@ export function New(){
         <Form>
           <header>
             <h1>Criar nota</h1>
-            < Link to="/">Voltar</ Link>
+            <ButtonText
+              title="Voltar"
+              onClick={handleBack}
+            />
           </header>
 
           <Input 
